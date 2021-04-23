d0e64b9e03d75eb12bec7a5129d965017ddec197
Rafael Ribeiro <romilson.2r@gmail.com>
2021-04-22 21:10:51 -0300

    First controller and Razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Developer"] = "ribeiro-dev";
+            ViewData["Professor"] = "Nélio Alves";
 
             return View();
         }
