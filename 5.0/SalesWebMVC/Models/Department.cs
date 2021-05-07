1b5106d72991b5dcd73d765579936a90cbc9c0e1
rafaelg <romilson.2r@gmail.com>
2021-05-07 17:10:35 -0300

    Other entities and second migration

--- 5.0/SalesWebMVC/Models/Department.cs
+++ 5.0/SalesWebMVC/Models/Department.cs
@@ -1,8 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+            // This constructor is only needed because we have another constructor with arguments
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(x => x.TotalSales(initial, final));
+        }
     }
 }
