064f82b0a516ff90e72bbb06ebb8609eb7722e2f
rafaelg <romilson.2r@gmail.com>
2021-05-11 13:47:35 -0300

    SellersController

---
+++ 5.0/SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellersController(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
