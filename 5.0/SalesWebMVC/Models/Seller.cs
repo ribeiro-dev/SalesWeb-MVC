4da28305906930571c48aae3031ec3c4b9f78eb7
rafaelg <romilson.2r@gmail.com>
2021-05-11 16:13:45 -0300

    Correct Seller variable name

--- 5.0/SalesWebMVC/Models/Seller.cs
+++ 5.0/SalesWebMVC/Models/Seller.cs
@@ -9,7 +9,7 @@ namespace SalesWebMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -19,12 +19,12 @@ namespace SalesWebMVC.Models
             // This constructor is only needed because we have another constructor with arguments
         }
 
-        public Seller(int id, string name, string email, DateTime date, double baseSalary, Department department)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
-            Date = date;
+            BirthDate = birthDate;
             BaseSalary = baseSalary;
             Department = department;
         }
