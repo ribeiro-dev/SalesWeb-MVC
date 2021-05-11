890769907f7867d3a8aeee6677d8d46f0e70e36f
rafaelg <romilson.2r@gmail.com>
2021-05-11 11:52:40 -0300

    Seeding service

--- 5.0/SalesWebMVC/Startup.cs
+++ 5.0/SalesWebMVC/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
+using SalesWebMVC.Data;
 
 namespace SalesWebMVC
 {
@@ -31,14 +32,18 @@ namespace SalesWebMVC
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"),
                     new MySqlServerVersion(new Version(8, 0, 24)),
                     builder => builder.MigrationsAssembly("SalesWebMVC")));
+
+            // adiciona serviços na injeção de dependencia
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
